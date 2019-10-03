feadc0e650eb04ec43aab48517d4f73096a03170
汪子惟 <m3iloveyoum3@gmail.com>
2019-10-03 15:21:13 +0800

    complete playfair decryptor

--- Hw1/Decrypt.py
+++ Hw1/Decrypt.py
@@ -1,5 +1,6 @@
 import abc
 import sys
+from typing import List
 
 CAESAR = 'caesar'
 PLAYFAIR = 'playfair'
@@ -17,7 +18,7 @@ class WrongInputException(Exception):
 class BaseDecryptor:
     def __init__(self, key: str, cipher_text: str):
         self.key = key
-        self.cipher_text = cipher_text
+        self.cipher_text = cipher_text.lower()
 
     @abc.abstractmethod
     def decrypt(self):
@@ -42,8 +43,76 @@ class CaesarDecryptor(BaseDecryptor):
 
 
 class PlayfairDecryptor(BaseDecryptor):
+    def __init__(self, key: str, cipher_text: str):
+        super(PlayfairDecryptor, self).__init__(key, cipher_text)
+        self.matrix = []
+
     def decrypt(self) -> str:
-        pass
+        self.make_matrix()
+        text = self.get_origin_char()
+        return text
+
+    def make_matrix(self):
+        self.key = self.key.lower()
+        for char in self.key:
+            if char == 'j':
+                char = 'i'
+
+            if char not in self.matrix:
+                self.matrix.append(char)
+
+        for alpha_code in range(ord('a'), ord('z') + 1):
+            char = chr(alpha_code)
+            if char == 'j':
+                char = 'i'
+
+            if char not in self.matrix:
+                self.matrix.append(char)
+
+            if len(self.matrix) >= 25:
+                break
+
+    def get_origin_char(self) -> str:
+        origin_str = ''
+
+        for char_index in range(0, len(self.cipher_text), 2):
+            char1 = self.cipher_text[char_index]
+            char2 = self.cipher_text[char_index + 1]
+
+            index1 = self._get_matrix_index_by(char1)
+            index2 = self._get_matrix_index_by(char2)
+
+            # same column
+            if index1 % 5 == index2 % 5:
+                char1 = self._get_matrix_char_by(index1 + 20)
+                char2 = self._get_matrix_char_by(index2 + 20)
+            # same row
+            elif int(index1 / 5) == int(index2 / 5):
+                char1 = self._get_matrix_char_by((index1 + 4) % 5 + self._get_row_index_by(index1) * 5)
+                char2 = self._get_matrix_char_by((index2 + 4) % 5 + self._get_row_index_by(index2) * 5)
+            else:
+                char1 = self._get_matrix_char_by(index2 % 5 + self._get_row_index_by(index1) * 5)
+                char2 = self._get_matrix_char_by(index1 % 5 + self._get_row_index_by(index2) * 5)
+
+            # if char2 == 'x':
+            #     char2 = char1
+
+            origin_str += char1 + char2
+
+        return origin_str
+
+    def _get_matrix_index_by(self, char) -> int:
+        return self.matrix.index(char)
+
+    def _get_matrix_char_by(self, index) -> str:
+        return self.matrix[index % 25]
+
+    @staticmethod
+    def _get_row_index_by(index) -> int:
+        return index // 5
+
+    def _get_head_by(self, index) -> int:
+        return int(index / 5) * 5
 
 
 class VernamDecryptor(BaseDecryptor):
