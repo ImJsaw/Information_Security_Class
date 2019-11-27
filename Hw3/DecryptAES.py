ccee7283ed55afa4459e1205cf5a160aa6a6b700
汪子惟 <m3iloveyoum3@gmail.com>
2019-11-27 21:25:52 +0800

    Hw3 - finish

--- Hw3/DecryptAES.py
+++ Hw3/DecryptAES.py
@@ -1,7 +1,6 @@
 import io
-import os
+from pathlib import Path
 
-from Crypto import Random
 from Crypto.Cipher import AES
 from PIL import Image
 
@@ -13,13 +12,15 @@ class DecryptAES:
         self._iv = self._key
         print('[**] Random key is', self._key)
 
-    def decrypt(self, plain_text):
+    def decrypt(self, cipher_text):
         if self._mode == ECB:
-            self._ecb_decrypt(plain_text=plain_text)
+            self._ecb_decrypt(cipher_text=cipher_text)
         elif self._mode == CBC:
-            self._cbc_decrypt(plain_text=plain_text)
+            self._cbc_decrypt(cipher_text=cipher_text)
+        elif self._mode == CBC_F:
+            self._cbc_feedback_decrypt(cipher_text=cipher_text)
 
-    def _ecb_decrypt(self, plain_text):
+    def _ecb_decrypt(self, cipher_text):
         count = 0
         count_newline = 0
         cipher = AES.new(self._key, AES.MODE_ECB)
@@ -27,7 +28,7 @@ class DecryptAES:
 
         # 把不需要加密的部分取出
         while count_newline < 3:
-            byte = plain_text[count]
+            byte = cipher_text[count]
             count += 1
             byte = bytes(chr(byte), encoding='utf-8')
             plainTxt += byte
@@ -38,16 +39,16 @@ class DecryptAES:
         block_index = 0
 
         # decrypt
-        plain_text = plain_text[count:]
-        while block_index < len(plain_text):
-            block = plain_text[block_index: block_index + AES.block_size]
+        cipher_text = cipher_text[count:]
+        while block_index < len(cipher_text):
+            block = cipher_text[block_index: block_index + AES.block_size]
             cipher_block = cipher.decrypt(block)
             plainTxt += cipher_block
 
             block_index += AES.block_size
         self.writeDecryptImg(plainTxt)
 
-    def _cbc_decrypt(self, plain_text):
+    def _cbc_decrypt(self, cipher_text):
         count = 0
         count_newline = 0
         cipher = AES.new(self._key, AES.MODE_ECB)
@@ -56,7 +57,7 @@ class DecryptAES:
 
         # 把不需要加密的部分取出
         while count_newline < 3:
-            byte = plain_text[count]
+            byte = cipher_text[count]
             count += 1
             byte = bytes(chr(byte), encoding='utf-8')
             plainTxt += byte
@@ -67,9 +68,9 @@ class DecryptAES:
         block_index = 0
 
         # decrypt
-        plain_text = plain_text[count:]
-        while block_index < len(plain_text):
-            block = plain_text[block_index: block_index + AES.block_size]
+        cipher_text = cipher_text[count:]
+        while block_index < len(cipher_text):
+            block = cipher_text[block_index: block_index + AES.block_size]
             plain_block = cipher.decrypt(block)
 
             # xor with prev cipher txt
@@ -80,12 +81,49 @@ class DecryptAES:
             block_index += AES.block_size
         self.writeDecryptImg(plainTxt)
 
+    def _cbc_feedback_decrypt(self, cipher_text):
+        count = 0
+        count_newline = 0
+        cipher = AES.new(self._key, AES.MODE_ECB)
+        plainTxt = b''
+        prev_ct = self._iv
+
+        # 把不需要加密的部分取出
+        while count_newline < 3:
+            byte = cipher_text[count]
+            count += 1
+            byte = bytes(chr(byte), encoding='utf-8')
+            plainTxt += byte
+
+            if byte == b'\n':
+                count_newline += 1
+
+        block_index = 0
+
+        # decrypt
+        cipher_text = cipher_text[count:]
+        while block_index < len(cipher_text):
+            block = cipher_text[block_index: block_index + AES.block_size]
+            plain_block = cipher.decrypt(block)
+
+            # xor with prev cipher txt
+            final_block = byte_xor(plain_block, prev_ct)
+            prev_ct = block[1:] + b'\x00'
+            plainTxt += final_block
+
+            block_index += AES.block_size
+        self.writeDecryptImg(plainTxt)
+
     def writeDecryptImg(self, plainTxt):
-        with open("./Hw3/ans.ppm", "wb") as f:
-            f.write(plainTxt)
-        ppm_picture = './Hw3/ans.ppm'
+        ans_path = Path(__file__).parent / 'ans.ppm'
+        with open(ans_path, "wb") as fp:
+            fp.write(plainTxt)
+
+        ppm_picture = ans_path
         output_img = Image.open(ppm_picture)
-        output_img.save('./Hw3/ans.png', 'png')
+
+        result_path = Path(__file__).parent / 'ans.png'
+        output_img.save(result_path, 'png')
 
 
 def byte_xor(ba1, ba2):
@@ -94,23 +132,29 @@ def byte_xor(ba1, ba2):
 
 ECB = 'ECB'
 CBC = 'CBC'
+CBC_F = 'CBC_F'
 
 
 if __name__ == '__main__':
     
     mode = input('輸入加密的Mode: ')
     #open cipher img
-    im = Image.open('./Hw3/result.ppm')
+    path = Path(__file__).parent / 'result.ppm'
+    im = Image.open(path)
     img_byte_array = io.BytesIO()
     im.save(img_byte_array, format=im.format)
     img_byte_array = img_byte_array.getvalue()
-    
-    with open('./Hw3/key.txt', 'rb') as f:
+
+    path = Path(__file__).parent / 'key.txt'
+    with open(path, 'rb') as f:
         key = f.read()
 
     if mode == ECB:
         decrypt_aes = DecryptAES(decrypt_mode=ECB, key=key)
-        decrypt_aes.decrypt(plain_text=img_byte_array)
+        decrypt_aes.decrypt(cipher_text=img_byte_array)
     elif mode == CBC:
         decrypt_aes = DecryptAES(decrypt_mode=CBC, key=key)
-        decrypt_aes.decrypt(plain_text=img_byte_array)
+        decrypt_aes.decrypt(cipher_text=img_byte_array)
+    elif mode == CBC_F:
+        decrypt_aes = DecryptAES(decrypt_mode=CBC_F, key=key)
+        decrypt_aes.decrypt(cipher_text=img_byte_array)
--- Hw3/EncryptAES.py
+++ Hw3/EncryptAES.py
@@ -11,7 +11,9 @@ class EncryptAES:
         self._mode = encrypt_mode
         self._key = Random.new().read(AES.block_size)
         self._iv = self._key
-        with open('./Hw3/key.txt', 'wb') as f:
+
+        key_path = Path(__file__).parent / 'key.txt'
+        with open(key_path, 'wb') as f:
             f.write(self._key)
 
     def encrypt(self, plain_text):
@@ -19,6 +21,8 @@ class EncryptAES:
             self._ecb_encrypt(plain_text=plain_text)
         elif self._mode == CBC:
             self._cbc_encrypt(plain_text=plain_text)
+        elif self._mode == CBC_F:
+            self._cbc_feedback_encrypt(plain_text=plain_text)
 
     def _ecb_encrypt(self, plain_text):
         count = 0
@@ -47,12 +51,7 @@ class EncryptAES:
 
             block_index += AES.block_size
 
-        with open("./Hw3/result.ppm", "wb") as f:
-            f.write(cipher_text)
-
-        ppm_picture = './Hw3/result.ppm'
-        output_img = Image.open(ppm_picture)
-        output_img.save('./Hw3/result.png', 'png')
+        self._write_image(cipher_text)
 
     def _cbc_encrypt(self, plain_text):
         count = 0
@@ -85,12 +84,52 @@ class EncryptAES:
 
             block_index += AES.block_size
 
-        with open("./Hw3/result.ppm", "wb") as f:
+        self._write_image(cipher_text)
+
+    def _cbc_feedback_encrypt(self, plain_text):
+        count = 0
+        count_newline = 0
+        cipher = AES.new(self._key, AES.MODE_ECB)
+        cipher_text = b''
+        prev_ct = self._iv
+
+        # 把不需要加密的部分取出
+        while count_newline < 3:
+            byte = plain_text[count]
+            count += 1
+            byte = bytes(chr(byte), encoding='utf-8')
+            cipher_text += byte
+
+            if byte == b'\n':
+                count_newline += 1
+
+        block_index = 0
+
+        # encrypt
+        plain_text = plain_text[count:]
+        while block_index < len(plain_text):
+            block = plain_text[block_index: block_index + AES.block_size]
+            final_block = byte_xor(block, prev_ct)
+
+            cipher_block = cipher.encrypt(final_block)
+            prev_ct = cipher_block[1:] + b'\x00'
+            cipher_text += cipher_block
+
+            block_index += AES.block_size
+
+        self._write_image(cipher_text)
+
+    @staticmethod
+    def _write_image(cipher_text):
+        ppm_path = Path(__file__).parent / 'result.ppm'
+        with open(ppm_path, "wb") as f:
             f.write(cipher_text)
 
-        ppm_picture = './Hw3/result.ppm'
+        ppm_picture = ppm_path
         output_img = Image.open(ppm_picture)
-        output_img.save('./Hw3/result.png', 'png')
+
+        result_path = Path(__file__).parent / 'result.png'
+        output_img.save(result_path, 'png')
 
 
 def byte_xor(ba1, ba2):
@@ -99,6 +138,7 @@ def byte_xor(ba1, ba2):
 
 ECB = 'ECB'
 CBC = 'CBC'
+CBC_F = 'CBC_F'
 
 
 if __name__ == '__main__':
@@ -115,4 +155,7 @@ if __name__ == '__main__':
         encrypt_aes.encrypt(plain_text=img_byte_array)
     elif mode == CBC:
         encrypt_aes = EncryptAES(encrypt_mode=CBC)
+        encrypt_aes.encrypt(plain_text=img_byte_array)
+    elif mode == CBC_F:
+        encrypt_aes = EncryptAES(encrypt_mode=CBC_F)
         encrypt_aes.encrypt(plain_text=img_byte_array)
