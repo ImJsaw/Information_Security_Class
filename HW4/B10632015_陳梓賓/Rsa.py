33e9390abeb3ef0d35947cebabde0f086c90646e
賓 <bing5300@gmail.com>
2019-12-02 12:38:36 +0800

    HW4 70分 version complete

--- HW4/B10632015_陳梓賓/Rsa.py
+++ HW4/B10632015_陳梓賓/Rsa.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 p = 0
 q = 0
@@ -12,45 +13,51 @@ def init():
     print("init")
     # generate prime nember p, q
     p = randomPrime()
-    #q = randomPrime()
-    q = 3
+    q = randomPrime()
     n = p * q
     phi_n = (p-1)*(q-1)
     e = findMinInterprime( phi_n )
     d = findInverse(e, phi_n)
+    print("p q n e d")
     print(p,q,n,e,d)
     return
 
 def encrypt():
     print("encrypt")
     x = input("input x : ")
-    plainTxt = []
-    cipherTxt = []
-    for i in x:
-        print("ori : ",i," ascii : ",ord(i))
-        plainTxt.append(ord(i))
-        yD = ( ord(i) ** (e % phi_n) ) % n
-        cipherTxt.append(yD)
-    print(plainTxt)
-    x = int(x)
     #y = x^e % n
     ##  y = x ^ (e%phi_n)  % n
-    y = ( x ** (e % phi_n) ) % n
+    y = ( int(x) ** (e % phi_n) ) % n
     print("y : ",y)
+    print("n : ",n)
+    print("d : ",d)
     return
 
 def decrypt():
     print("decrypt")
     y = int( input("input y : ") )
+    
+    n = int( input("input n : ") )
+    d = int( input("input d : ") )
     #x = y^d % n
     ##  x = y ^ (d%phi_n)  % n
-    x = ( y ** (d % phi_n) ) % n
+    x = ( y ** d ) % n
     print("x : ",x)
     return
 
 def randomPrime():
-    #TODO:
-    return 11
+    min = 2
+    max  = 100
+    # random choose prime between the range
+    primes = [i for i in range(min,max) if isPrime(i)]
+    return random.choice(primes)
+
+def isPrime(primeTesting):
+    #test every num < test^0.5
+    for i in range(1, math.ceil( primeTesting ** 0.5 + 0.1 )):
+        if math.gcd(i, primeTesting) != 1:
+            return False
+    return True
 
 #找到最小互質
 def findMinInterprime(a):
@@ -70,11 +77,10 @@ def findInverse( base , mod ):
 
 mode = input('輸入動作代碼 1/加密 2/解密 :  ')
 
-#init first
-init()
-
 if mode == "1":
-   encrypt()
+    #init first
+    init()
+    encrypt()
 elif mode == "2":
     decrypt()
     
