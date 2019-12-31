c4865db12b3f93ea3f3f15bc5c8d3c1d5272db21
汪子惟 <m3iloveyoum3@gmail.com>
2019-12-31 19:17:49 +0800

    Hw5 - generate public key & private key

--- Hw5/Dsa.py
+++ Hw5/Dsa.py
@@ -6,18 +6,52 @@ class DSA:
         print("-------------------------------------- init -------------------------------------")
         # generate prime number p, q
         self.p, self.q = self._generate_p_and_q()
-        print(self.q)
+        print(f'p:{self.p}\nq:{self.q}')
+        self.a = self._generate_a()
+        print(f'a:{self.a}')
+        self.d = random.randint(1, self.q)
+        print(f'd:{self.d}')
+        self.b = self._generate_b()
+        print(f'b:{self.b}')
 
-    def _generate_p_and_q(self):
+    def _generate_b(self):
+        b = square_and_multiply(self.a, self.d, self.p)
+        return b
+
+    def _generate_a(self):
+        h = random.randint(1, self.p - 1)
+        exp = (self.p - 1) // self.q
+        a = square_and_multiply(h, exp, self.p)
+        return a
+
+    @staticmethod
+    def _generate_p_and_q() -> tuple:
         while True:
             q = get_random_big_prime(bit_no=160)
-            
-            continue
+
+            big_no = ''
+            # generate 864 bits big number
+            for _ in range(864):
+                big_no += '1'
+            big_no = int(big_no, 2)
+
+            big_no_base = ''
+            for _ in range(863):
+                big_no_base += '1'
+            big_no_base = int(big_no_base, 2)
+
+            for i in range(big_no_base, big_no):
+                p = q * i + 1
+                if miller_rabin_test(p, 300):
+                    return p, q
+
+            print('p not found')
+
 
 def get_random_big_prime(bit_no):
     while 1 == 1:
         big_no = "1"
-        for _ in range(bit_no-2):
+        for _ in range(bit_no - 2):
             if random.random() > 0.5:
                 big_no += "1"
             else:
@@ -30,23 +64,23 @@ def get_random_big_prime(bit_no):
 
 
 def miller_rabin_test(n, k):
-    r = n-1
+    r = n - 1
     u = 0
     while r % 2 == 0:
         r //= 2
         u += 1
 
     for _ in range(k):
-        witness = random.randint(2, n-1)
+        witness = random.randint(2, n - 1)
         res = square_and_multiply(witness, r, n)
 
-        if res == 1 or res == n-1:
+        if res == 1 or res == n - 1:
             continue
 
         pass_flag = False
-        for _ in range(u-1):
+        for _ in range(u - 1):
             res = (res ** 2) % n
-            if res == n-1:
+            if res == n - 1:
                 pass_flag = True
                 break
         if pass_flag:
@@ -63,12 +97,11 @@ def square_and_multiply(base, exp, mod):
     for i in bin_exp:
         ans = (ans ** 2) % mod
         if i == '1':
-            ans = (ans*base) % mod
+            ans = (ans * base) % mod
     return ans
 
 
 if __name__ == '__main__':
-
     # mode = input('輸入指令 encrypt/加密 decrypt/解密 :  ')
 
     dsa = DSA()
